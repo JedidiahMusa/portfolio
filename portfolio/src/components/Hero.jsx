ceb906db749dca574309419b5b2c0f1d0d4d30c6
JedidiahMusa <jedidiahsimon3@gmail.com>
2025-06-25 17:15:59 +0100

    proper progress

---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import picture from "../assets/mypic.png"
+
+function Hero() {
+  return (
+    <div className="top-0 left-0 flex bg-black justify-center grid-background w-full min-h-[90vh]">
+      <div className='w-[90%] flex-col-reverse md:grid grid-cols-2 grid-rows-1'>
+
+        <div className='w-[90%] md:w-full flex items-center justify-center md:h-full rounded-full '>
+          <img src={picture} alt="" />
+        </div>
+
+        <div className='flex flex-col items-center min-h-[50vh] md:h-full justify-center md:items-start w-full '>
+          <p className='text-white text-5xl my-8 '>Hi i'm <b className='p-3 rounded-full shadow-md shadow-red-400 bg-red-500'>Jedidiah</b> </p>
+          <p className='text-white text-4xl mb-4 '>a web developer</p>
+          <p className='text-white px-12 md:px-0 text-center md:text-left '>I specialize in transforming complex designs into visually captivating, user-friendly websites that engage and inspire audiences.</p>
+        </div>
+
+
+        <div className=' w-full h-fit '></div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Hero
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,23 +1,33 @@
-import { Spiral as Hamburger } from "hamburger-react";
+import { Sling as Hamburger } from "hamburger-react";
 import { useState } from "react";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
 
 function Navbar() {
   const [isOpen, setOpen] = useState(false);
+  const year = Date.now;
 
   return (
     <>
-      <div className="w-full flex items-center justify-center h-20">
-        <nav className="h-[80%] w-[90%] flex justify-between items-center rounded-3xl px-4 bg-gray-400 ">
-          <p className="font-bold text-white text-2xl ">JED</p>
+      <div className="w-full pt-8 bg-black ">
+        <nav className="h-16 w-[90%] mx-auto flex justify-between items-center rounded-3xl px-4 bg-white ">
+          <p className="font-bold text-black text-2xl ">JED</p>
 
           <ul className="md:flex gap-4 hidden">
-            <li>About Me</li>
+            <li>About</li>
             <li>Projects</li>
             <li>Contact</li>
           </ul>
 
+            <button className="hidden md:flex cursor-pointer shadow-md shadow-red-400 text-white hover:scale-105 bg-red-500 p-2 rounded-full font-semibold">Hire Me</button>
+          
           <div className="md:hidden z-10 ">
-            <Hamburger color="white" toggled={isOpen} toggle={setOpen} />
+            {isOpen ? (
+              <Hamburger color="white" toggled={isOpen} toggle={setOpen} />
+            ) : (
+              <Hamburger color="black" toggled={isOpen} toggle={setOpen} />
+            )}
           </div>
         </nav>
       </div>
@@ -25,9 +35,36 @@ function Navbar() {
       {isOpen ? (
         <div className="w-full h-screen absolute top-0 bg-[#000000ec]">
           <ul className="text-white px-4 pt-24 ">
-            <li className="py-6 w-full flex items-center justify-center border rounded-4xl my-4 border-gray-500 font-semibold text-2xl ">About Me</li>
-            <li className="py-6 w-full flex items-center justify-center border rounded-4xl my-4 border-gray-500 font-semibold text-2xl ">Projects</li>
-            <li className="py-6 w-full flex items-center justify-center border rounded-4xl my-4 border-gray-500 font-semibold text-2xl ">Contact</li>
+            <li className="py-6 w-full flex items-center justify-center border rounded-4xl my-4 border-gray-500 font-semibold text-2xl ">
+              About Me
+            </li>
+            <li className="py-6 w-full flex items-center justify-center border rounded-4xl my-4 border-gray-500 font-semibold text-2xl ">
+              Projects
+            </li>
+            <li className="py-6 w-full flex items-center justify-center border rounded-4xl my-4 border-gray-500 font-semibold text-2xl ">
+              Contact
+            </li>
+             <li className="py-6 w-full flex items-center justify-center border rounded-4xl my-4 border-gray-500 font-semibold text-2xl ">
+              Hire Me
+            </li>
+            <div className="py-6 w-full flex items-center justify-between px-20 my-4 border-gray-500 font-semibold text-2xl">
+              <FaInstagram
+                size={30}
+                className="hover:scale-105 hover:text-gray-400 "
+              />
+              <FaXTwitter
+                size={30}
+                className="hover:scale-105 hover:text-gray-400 "
+              />
+              <FaLinkedin
+                size={30}
+                className="hover:scale-105 hover:text-gray-400 "
+              />
+              <IoIosMail
+                size={30}
+                className="hover:scale-105 hover:text-gray-400 "
+              />
+            </div>
           </ul>
         </div>
       ) : (
