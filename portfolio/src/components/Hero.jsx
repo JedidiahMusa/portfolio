5e7376cd3ac5b663a6201330cead41450089d3de
JedidiahMusa <jedidiahsimon3@gmail.com>
2025-07-07 16:05:39 +0100

    ,

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import picture from "../assets/mypic.png"
+import React from "react";
+import picture from "../assets/mypic.png";
 
 function Hero() {
   return (
     <div className="top-0 left-0 flex bg-black justify-center grid-background w-full min-h-[90vh]">
-      <div className='w-[90%] flex-col-reverse md:grid grid-cols-2 grid-rows-1'>
-
-        <div className='w-[90%] md:w-full flex items-center justify-center md:h-full rounded-full '>
-          <img src={picture} alt="" />
+      <div className="w-[90%] flex py-8 flex-col-reverse md:grid grid-cols-2 grid-rows-1">
+        <div className="flex flex-col items-center min-h-[50vh] md:h-full justify-center md:items-start w-full ">
+          <p className="text-white text-5xl my-8 ">
+            Hi i'm{" "}
+            <button className="p-3 text-[] rounded-full cursor-pointer hover:scale-110 centered-shadow ">
+              Jedidiah
+            </button>{" "}
+          </p>
+          <p className="text-white text-4xl mb-4 ">a web developer</p>
+          <p className="text-white px-12 md:px-0 text-center md:text-left ">
+            I specialize in transforming complex designs into visually
+            captivating, user-friendly websites that engage and inspire
+            audiences.
+          </p>
+          <button
+            className=".
+          cursor-pointer text-white hover:scale-105 bg-red-500 p-2 rounded-full font-semibold"
+          >
+            Download CV
+          </button>
         </div>
 
-        <div className='flex flex-col items-center min-h-[50vh] md:h-full justify-center md:items-start w-full '>
-          <p className='text-white text-5xl my-8 '>Hi i'm <b className='p-3 rounded-full shadow-md shadow-red-400 bg-red-500'>Jedidiah</b> </p>
-          <p className='text-white text-4xl mb-4 '>a web developer</p>
-          <p className='text-white px-12 md:px-0 text-center md:text-left '>I specialize in transforming complex designs into visually captivating, user-friendly websites that engage and inspire audiences.</p>
-          <button className=".
-          cursor-pointer shadow-md shadow-red-400 text-white hover:scale-105 bg-red-500 p-2 rounded-full font-semibold">Download CV</button>
+        <div className="h-full w-full flex items-center justify-center ">
+          <div className="w-[25rem] h-[25rem] border-1 centered-shadow overflow-hidden border-red-300 rounded-full ">
+            <img src={picture} alt="" className="object-contain w-full h-full " />
+          </div>
         </div>
 
-
-        <div className=' w-full h-fit '></div>
+        
       </div>
     </div>
-    
-  )
+  );
 }
 
-export default Hero
+export default Hero;
